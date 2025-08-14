e565d4207286fb9b0d7c4166cc30c7a70500f9a6
q199255 <q199255@gmail.com>
2025-08-14 17:10:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def stream():
-    url = os.environ.get("STREAM_URL", "https://a42ee3cb9fb5.ngrok-free.app/stream.mp3")
+    url = os.environ.get("STREAM_URL", "https://d4c3c726c051.ngrok-free.app/stream.mp3")
     headers = {
         'ngrok-skip-browser-warning': '1'
     }
